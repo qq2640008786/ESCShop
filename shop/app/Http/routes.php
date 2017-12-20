6dde786afc393f927ddf67a40a3341a1bc2c4e2d
廖柯 <2640008786@qq.com>
2017-12-20 09:33:02 +0800

    路由配置

--- shop/app/Http/routes.php
+++ shop/app/Http/routes.php
@@ -10,7 +10,15 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+// 后台登录模块
+Route::get('login','Admin\LoginController@login');
+// 后台进入index模块
+Route::post('/login','Admin\LoginController@login');
+Route::get('index','Admin\IndexController@index');
+Route::get('login_out','Admin\LoginController@login_out');
+Route::get('goods_list','Admin\goodsController@goods_list');
+Route::get('goods_add','Admin\goodsController@goods_add');
+Route::get('species_list','Admin\speciesController@species_list');
+Route::get('species_add','Admin\speciesController@species_add');
+Route::get('role_list','Admin\roleController@role_list');
+Route::get('role_add','Admin\roleController@role_add');

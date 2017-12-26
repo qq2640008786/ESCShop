519ddedd786338ab6e0020fa3bf1fce07446769d
廖柯 <2640008786@qq.com>
2017-12-26 11:12:17 +0800

    修改静态资源

--- shop/config/filesystems.php
+++ shop/config/filesystems.php
@@ -47,7 +47,10 @@ return [
             'driver' => 'local',
             'root'   => storage_path('app'),
         ],
-
+        'upload' => [
+            'driver' => 'local',
+            'root' => public_path('uploads'),
+        ],
         'ftp' => [
             'driver'   => 'ftp',
             'host'     => 'ftp.example.com',
